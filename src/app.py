567f9cdab325c055462641e4ee9be0ec9edb3a9f
Kevin Santos <kevin.curado.001@student.uni.lu>
2024-06-14 01:57:01 +0200

    Example app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,38 @@
 import discord
+import os
+import random
+from dotenv import load_dotenv
 
-bot = discord.Client()
+load_dotenv()
+TOKEN = os.getenv("TOKEN_BOT")
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return 
+    
+    username = str(message.author)
+    user_message = message.content
+    channel = str(message.channel)
+
+    print(f'[{channel}] {username}: {user_message}')
+    
+    if user_message == 'ping':
+        await message.channel.send('pong')
+    else:
+        print('error')
+
+def main():
+    client.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
