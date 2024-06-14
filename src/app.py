c8b22b4c306c53d25ca2fc4b59895ac4051369c7
Kevin Santos <kevin.curado.001@student.uni.lu>
2024-06-14 08:50:04 +0200

    WE HAVE MUSIC IN VOICE FROM YOUTUBE

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
 
-from utils.commands import Music
+from utils.music import MusicBot
 
 discord.utils.setup_logging()
 
@@ -13,7 +13,7 @@ TOKEN = os.getenv("TOKEN_BOT")
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+intents.voice_states = True
 
 bot = commands.Bot(
         command_prefix=commands.when_mentioned_or('!'),
@@ -40,7 +40,7 @@ async def on_ready():
 
 async def main():
     async with bot:
-        await bot.add_cog(Music(bot))
+        await bot.add_cog(MusicBot(bot))
         await bot.start(TOKEN)
 
 if __name__ == "__main__":
---
+++ src/utils/music.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+import yt_dlp
+
+
+ffmpeg_opt = {'options': '-vn'}
+ydl_opt = {'format': 'bestaudio', 'noplaylist': True}
+
+class MusicBot(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.queue = []
+
+    @commands.command()
+    async def play(self, ctx, *, search):
+        voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+        if not voice_channel:
+            return await ctx.send("You are not in a voice channel!")
+        if not ctx.voice_client:
+            await voice_channel.connect()
+
+        async with ctx.typing():
+            with yt_dlp.YoutubeDL(ydl_opt) as ydl:
+                info = ydl.extract_info(f"ytsearch:{search}", download=False)
+                if 'entries' in info:
+                    info = info['entries'][0]
+                url = info['url']
+                title = info['title']
+                self.queue.append((url,title))
+                await ctx.send(f'Added t queue **{title}**')
+        if not ctx.voice_client.is_playing():
+            await self.play_next(ctx)
+
+    async def play_next(self, ctx):
+        if self.queue:
+            url, title = self.queue.pop(0)
+            source = await discord.FFmpegOpusAudio.from_probe(url, **ffmpeg_opt)
+            ctx.voice_client.play(source, after=lambda _: self.client.loop.asyncio.create_task(self.play_next(ctx)))
+            await ctx.send(f'Now playing **{title}**')
+        elif not ctx.voice_client.is_playing():
+            await ctx.send("Queue os empty!")
+
+    @commands.command()
+    async def skip(self, ctx):
+        if ctx.voice_client and ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+            await ctx.send("stopped")
+
+    @commands.command()
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
+
+
