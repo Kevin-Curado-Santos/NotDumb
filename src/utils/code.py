fbe0e29b1c02c21bc8d97a7f4a48337bdcbead9c
Kevin Santos <kevin.curado.001@student.uni.lu>
2024-06-15 00:37:12 +0200

    codeforces contests command

--- src/utils/code.py
+++ src/utils/code.py
@@ -7,6 +7,8 @@ from discord.ext import commands
 
 problemset_api = "https://codeforces.com/api/problemset.problems"
 problemset_url = "https://codeforces.com/problemset/problem"
+contests_url = "https://codeforces.com/api/contest.list"
+contest_url = "https://codeforces.com/contest"
 
 def check_problem(rating):
     return (lambda problem: "contestId" in problem and "rating" in problem and problem["rating"] >= rating[0] and problem["rating"] <= rating[1])
@@ -21,6 +23,16 @@ def process_response(request):
     result = json.loads(result)
     problems = result["result"]["problems"]
     return problems
+
+def future_contests(request):
+    result = request.text
+    result = json.loads(result)
+    contests = []
+    i = 0
+    while result["result"][i]["phase"] == "BEFORE":
+        contests.append([result["result"][i]["id"], result["result"][i]["name"]])   
+        i+=1
+    return contests[::-1]
 
 class Codebot(commands.Cog):
     def __init__(self, client):
@@ -71,6 +83,17 @@ class Codebot(commands.Cog):
 
 
 
+    @commands.command()
+    async def contests(self, ctx):
+        query_url = contests_url
+        request = requests.get(query_url)
+        contests = future_contests(request)
+        emb = discord.Embed(title='Upcoming Contest')
+        emb.set_thumbnail(url='https://codeforces.org/s/14108/images/codeforces-sponsored-by-ton.png')
+        for c in contests:
+            _id, name = c[0], c[1]
+            url = contest_url+f"/{_id}"
+            emb.add_field(name=name, value=url, inline=False)
 
-
+        await ctx.send(embed=emb)
 
