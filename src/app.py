ba089a16f3109d6a20c312670b89b93e54c0d62a
Kevin Santos <kevin.curado.001@student.uni.lu>
2024-06-14 07:58:21 +0200

    A try to voice interaction

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 import discord
 import os
-import random
+from discord.ext import commands
 from dotenv import load_dotenv
+import asyncio
+
+from utils.commands import Music
+
+discord.utils.setup_logging()
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN_BOT")
@@ -9,30 +14,34 @@ TOKEN = os.getenv("TOKEN_BOT")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(
+        command_prefix=commands.when_mentioned_or('!'),
+        intents=intents,
+)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return 
-    
-    username = str(message.author)
-    user_message = message.content
-    channel = str(message.channel)
+#async def on_message(message):
+#    if message.author == bot.user:
+#        return 
+#    
+#    username = str(message.author)
+#    user_message = message.content
+#    channel = str(message.channel)
+#
+#    print(f'[{channel}] {username}: {user_message}')
+#    
+#    if user_message == 'ping':
+#        await message.channel.send('pong')
 
-    print(f'[{channel}] {username}: {user_message}')
-    
-    if user_message == 'ping':
-        await message.channel.send('pong')
-    else:
-        print('error')
 
-def main():
-    client.run(TOKEN)
+async def main():
+    async with bot:
+        await bot.add_cog(Music(bot))
+        await bot.start(TOKEN)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
