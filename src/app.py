215955fbb3fafbf0bd78740ba4c55b3e552fbbf1
Kevin Santos <kevin.curado.001@student.uni.lu>
2024-06-16 23:29:26 +0200

    Balls command

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from utils.music import MusicBot
 from utils.dnd import Dndbot
 from utils.code import Codebot
 from utils.management import Management
+from utils.commands import Commands
 
 discord.utils.setup_logging()
 
@@ -47,6 +48,7 @@ async def main():
         await bot.add_cog(Dndbot(bot))
         await bot.add_cog(Codebot(bot))
         await bot.add_cog(Management(bot))
+        await bot.add_cog(Commands(bot))
         await bot.start(TOKEN)
 
 if __name__ == "__main__":
--- src/utils/commands.py
+++ src/utils/commands.py
@@ -1,1 +1,15 @@
+import discord
 
+from discord.ext import commands
+
+class Commands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.stickers = []
+    
+    @commands.command()
+    async def balls(self, ctx):
+        if not self.stickers:
+            self.stickers = ctx.guild.stickers
+        
+        await ctx.send(stickers=[self.stickers[7]])
